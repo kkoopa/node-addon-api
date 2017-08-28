3128d5a8722525a9aa459311d82fcd10e87e82ad
Anna Henningsen <anna@addaleax.net>
2017-08-28 17:09:34 +0200

    Refactor property setting
    Previously, various utility overloads of `Object::Set()` were provided.
    This was problematic, because accidental unexpected conversions
    could happen (e.g. `const char*` to `bool`) and overloads were
    ambiguous (e.g. `int` could be cast to `double` and `bool` and therefore
    could not be passed directly).
    
    To address this, pass a template parameter instead, and introduce
    a helper `Value::From` method that could create a value based on the
    type of object it receives, and that uses clear precedence rules for
    determining the kind of JS value to create.
    
    Fixes: https://github.com/nodejs/node-addon-api/issues/86

--- test/object.cc
+++ test/object.cc
@@ -102,6 +102,28 @@ void SetProperty(const CallbackInfo& info) {
   obj.Set(name, value);
 }
 
+Value CreateObjectUsingMagic(const CallbackInfo& info) {
+  Env env = info.Env();
+  Object obj = Object::New(env);
+  obj["cp_false"] = false;
+  obj["cp_true"] = true;
+  obj[std::string("s_true")] = true;
+  obj[std::string("s_false")] = false;
+  obj["0"] = 0;
+  obj[42] = 120;
+  obj["0.0f"] = 0.0f;
+  obj["0.0"] = 0.0;
+  obj["-1"] = -1;
+  obj["foo2"] = u"foo";
+  obj[std::string("foo4")] = NAPI_WIDE_TEXT("foo");
+  obj[std::string("foo5")] = "foo";
+  obj[std::string("foo6")] = std::u16string(NAPI_WIDE_TEXT("foo"));
+  obj[std::string("foo7")] = std::string("foo");
+  obj[std::string("circular")] = obj;
+  obj["circular2"] = obj;
+  return obj;
+}
+
 Object InitObject(Env env) {
   Object exports = Object::New(env);
 
@@ -110,6 +132,7 @@ Object InitObject(Env env) {
   exports["defineValueProperty"] = Function::New(env, DefineValueProperty);
   exports["getProperty"] = Function::New(env, GetProperty);
   exports["setProperty"] = Function::New(env, SetProperty);
+  exports["createObjectUsingMagic"] = Function::New(env, CreateObjectUsingMagic);
 
   return exports;
 }
